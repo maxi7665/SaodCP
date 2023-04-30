e977cd1cccc5ae2df52c1ded265b7a94717569d6
maxi7665 <maxi7665@gmail.com>
2023-04-30 21:57:45 +0300

    Lodger model created; Test Initial Commit

---
+++ SaodCP/Models/Lodger.cs
@@ -0,0 +1,20 @@
+﻿namespace SaodCP.Models
+{
+    /// <summary>
+    /// Постоялец
+    /// </summary>
+    public class Lodger
+    {
+        public static readonly string PassportIdPattern = "NNNN-NNNNNN";
+
+        public int PassportId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int BirthYear { get; set; }
+
+        public string Address { get; set; } = string.Empty;
+
+        
+    }
+}
---
+++ SaodCP/Utils/Utils.cs
@@ -0,0 +1,91 @@
+﻿using SaodCP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaodCP.Utils
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Проверка формата номера паспорта
+        /// </summary>
+        /// <param name="passportId">Номер паспорта</param>
+        /// <param name="error">Описание ошибки</param>
+        public static bool ValidateLodgerPassportId(
+            string? passportId,
+            ref string? error)
+        {
+            if (string.IsNullOrWhiteSpace(passportId))
+            {
+                error = "Номер паспорта должен быть заполнен";
+
+                return false;
+            }
+
+            var ret = true;
+
+            if (passportId.Length != 11)
+            {
+                ret = false;
+            }
+
+            var allowedCharactersArray = new char[]
+            {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+            };
+
+            var delimiter = '-';
+
+            if (ret)
+            {
+                for (var i = 0; i < passportId.Length; i++)
+                {
+                    var c = passportId[0];
+
+                    if (i == 4)
+                    {
+                        if (c == delimiter)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            ret = false;
+
+                            break;
+                        }
+                    }
+
+                    var found = false;
+
+                    for (var j = 1; j < allowedCharactersArray.Length; j++)
+                    {
+                        if (c == allowedCharactersArray[j])
+                        {
+                            found = true;
+
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        ret = false;
+
+                        break;
+                    }
+                }
+            }
+
+            if (!ret)
+            {
+                error = $"Номер паспорта должен соответствовать шаблону {Lodger.PassportIdPattern}";
+            }
+
+            return ret;
+        }
+    }
+}
